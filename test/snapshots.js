9516b51dc8f89a33b033fbcbabd7a37d0c97b74b
Philipp Krüger <philipp.krueger1@gmail.com>
2021-01-27 13:54:46 +0100

    :sparkles: Better snapshot organization

--- test/snapshots.js
+++ test/snapshots.js
@@ -5,41 +5,47 @@ import tailwindui from "@tailwindcss/ui";
 import test from "ava";
 
 
-test("snapshot of generated module", async assert => {
+test("snapshot of generated module", async t => {
     const generatedModule = await elmTailwindOrigami({
         directory: "./test/src/",
         moduleName: "Tailwind.Basic",
         postcssPlugins: [],
         tailwindConfig,
+        debugFunction: () => { },
         skipSaving: true,
     });
 
-    assert.snapshot(generatedModule);
+    t.snapshot(generatedModule.utilitiesModule, { id: t.title + ": Utilities Module" });
+    t.snapshot(generatedModule.breakpointsModule, { id: t.title + ": Breakpoints Module" });
 });
 
 
-test("snapshot of generated module with autoprefixer", async assert => {
+test("snapshot of generated module with autoprefixer", async t => {
     const generatedModule = await elmTailwindOrigami({
         directory: "./test/src/",
         moduleName: "Tailwind.WithAutoprefixer",
         postcssPlugins: [autoprefixer],
         tailwindConfig,
+        debugFunction: () => { },
         skipSaving: true,
     });
 
-    assert.snapshot(generatedModule);
+    t.snapshot(generatedModule.utilitiesModule, { id: t.title + ": Utilities Module" });
+    t.snapshot(generatedModule.breakpointsModule, { id: t.title + ": Breakpoints Module" });
 });
 
 
-test("snapshot of generated module with tailwindui", async assert => {
+test("snapshot of generated module with tailwindui", async t => {
     const generatedModule = await elmTailwindOrigami({
         directory: "./test/src/",
         moduleName: "Tailwind.WithAutoprefixer",
         postcssPlugins: [tailwindui],
         tailwindConfig,
+        debugFunction: () => { },
         skipSaving: true,
     });
 
-    assert.snapshot(generatedModule);
+    t.snapshot(generatedModule.utilitiesModule, { id: t.title + ": Utilities Module" });
+    t.snapshot(generatedModule.breakpointsModule, { id: t.title + ": Breakpoints Module" });
 });
 
