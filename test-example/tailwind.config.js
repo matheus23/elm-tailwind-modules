cb72f69511b3260aa9cb45a0a9424f96c585b198
Leo jPod <leo.jpod@lilo.org>
2022-07-27 21:39:44 +0200

    support for tailwindv3

--- test-example/tailwind.config.js
+++ test-example/tailwind.config.js
@@ -1,12 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    theme: {},
-    variants: [], // We can do variants in elm code
+    theme: {
+        extend: {},
+    },
+    variants: [],
     plugins: [
         require("@tailwindcss/typography"),
         require("@tailwindcss/forms"),
         require("@tailwindcss/aspect-ratio")
     ],
-    future: {
-        removeDeprecatedGapUtilities: true,
-    },
-};
+}
